93428086452dc9de6cd516d2feb7314b1b5f6dc0
wiwatyooraks <78940910+wiwatyooraks@users.noreply.github.com>
2021-04-04 17:08:58 +0700

    Angular2lab3

---
+++ app/product-item/product-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+  @Input() 
+  product:Product ;
+
+  @Output()
+  OnSelected: EventEmitter<Product> = new EventEmitter() ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  select() {
+    this.OnSelected.emit(this.product);
+    }
+   
+}
---
+++ app/product-list/product-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products : Product[];
+
+  constructor() { 
+    this.products = [];
+    this.products.push({
+    name: 'ส้มโอ',
+    price: 990
+    });
+    this.products.push({
+    name: 'แตงโม',
+    price: 1000
+    });
+    this.products.push({
+    name: 'มะพร้าวนํ้าหอม',
+    price: 500
+    });
+    }
+
+  ngOnInit(): void {
+  }
+  selectedProduct(product: Product) {
+    alert(`Product ${product.name} selected`);
+  }
+}
